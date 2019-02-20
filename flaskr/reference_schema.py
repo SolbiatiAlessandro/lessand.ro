09a0fe1c17e24d9944bc6c090a9e9c276164fb49
Solbiati Alessandro <alexsolbiati@hotmail.it>
2019-02-20 01:41:03 +0100

    updated a correct syntax for selecting from psql db, (what a ugly and convoluted syntax)

--- flaskr/reference_schema.py
+++ flaskr/reference_schema.py
@@ -3,7 +3,15 @@ import psycopg2
 DATABASE_URL = os.environ['DATABASE_URL']
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-db = conn.cursor()                                              db.execute("INSERT INTO users (username, password) VALUES ({}, {})".format("'test'","'test'"))
+db = conn.cursor()
+
+# this is the syntax to insert
+db.execute("INSERT INTO users (username, password) VALUES ({}, {})".format("'test'","'test'"))
 conn.commit()
+
+# this is the syntax to select
+db.execute("SELECT id FROM users WHERE username = '{}';".format(username))}
+print db.fetchone()
+
 db.close()
 conn.close()
