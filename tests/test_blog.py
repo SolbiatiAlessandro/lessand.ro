4d248980af648d1165101954bab9d83d640232dd
Solbiati Alessandro <alexsolbiati@hotmail.it>
2019-02-21 20:35:06 +0100

    [INTEGRATION] - I deleted assertion in tests on login and register button

--- tests/test_blog.py
+++ tests/test_blog.py
@@ -4,12 +4,13 @@ from flaskr.db import get_db
 
 def test_index(client, auth):
     response = client.get('/')
-    assert b"Log In" in response.data
-    assert b"Register" in response.data
+    # I don't want LogIn to be shown in main page
+    # assert b"Log In" in response.data
+    # assert b"Register" in response.data
 
     auth.login()
     response = client.get('/')
-    assert b'Log Out' in response.data
+    #assert b'Log Out' in response.data
     assert b'test title' in response.data
     assert b'by test on 2018-01-01' in response.data
     assert b'test body' in response.data
